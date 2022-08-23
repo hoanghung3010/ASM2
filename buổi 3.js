72bee7ed956a6f626a656f629d49c462a4fb2b7a
hoanghung3010 <hunglhhbdaf200040@fpt.edu.vn>
2022-08-23 13:19:50 +0700

    first commit

---
+++ buổi 3.js
@@ -0,0 +1,29 @@
+// var ok = confirm("Bạn chắc chắn muốn xóa chứ");
+// if(ok == true){
+//     doSomething();
+// }
+// else{
+//     doAnythingElse();
+// }
+
+// function xacNhan(traloi){
+//     var KetQua = "";
+//     if (traloi){
+//         KetQua = "Tuyet Voi.Chuc Bạn Chien thang!";
+//     }else{
+//         KetQua ="Hẹn Gặp Lại Bạn nhé!";
+//     }
+//     return KetQua;
+// }
+
+
+// var traloi = confirm("Bạn sẽ chơi game chứ");
+// var thongbao = xacNhan(traloi);
+// alert (thongbao)
+
+
+function hienthianh(convat){
+    if (convat == "cho"){
+        document.write("<img src ='281291456_1080666119541085_177876336213211749_n.jpg'>");
+    }
+}
